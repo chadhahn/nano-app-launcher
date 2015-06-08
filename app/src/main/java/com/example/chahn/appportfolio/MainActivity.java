807696244994d253d7fcbc4ae693d8fb8474c97e
Chad Hahn <chad.hahn@cnoinc.com>
2015-06-08 08:46:53 -0400

    Add comments. Removed unused(?) dimens file

--- app/src/main/java/com/example/chahn/appportfolio/MainActivity.java
+++ app/src/main/java/com/example/chahn/appportfolio/MainActivity.java
@@ -7,8 +7,14 @@ import android.widget.Toast;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Main activity class
+ */
 public class MainActivity extends AppCompatActivity {
 
+  /**
+   * Constant map to hold View -> Toast String. Static final valid like this inside Activity?
+   */
   private static final Map<Integer, Integer> TOAST_MAP = new HashMap<>();
 
   static {
@@ -20,16 +26,20 @@ public class MainActivity extends AppCompatActivity {
     TOAST_MAP.put(R.id.capstone_button, R.string.capstone_toast);
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
 
+  /**
+   * Render a Toast based on View that's clicked. Look up the Toast String via HashMap that holds
+   * View -> String resource id.
+   *
+   * @param view The view (button) that was clicked on
+   */
   public void mainButtonClick(View view) {
     Integer toastResourceId = TOAST_MAP.get(view.getId());
     String msg = getResources().getString(toastResourceId);
     Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
   }
-
 }
